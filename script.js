7e6468fd07bbfbe38ee1ed2a8aaa98f4aef6c7e9
alexzma <alexanderzma@hotmail.com>
2019-02-17 15:25:55 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 
 window.addEventListener('load', function load(event){
-    console.log("Popup DOM fully loaded and parsed");
-
+	    console.log("Popup DOM fully loaded and parsed");
     function modifyDOM() {
         //You can play with your DOM here or check URL against your regex
         console.log('Tab script:');
         console.log(document.body);
-        return document.body.innerHTML;
+        if(document.URL === "https://be.my.ucla.edu/studylist.aspx")
+	        return document.getElementById("div_landing").innerHTML;
     }
 
     //We have permission to access the activeTab, so we can call chrome.tabs.executeScript:
@@ -16,6 +16,7 @@ window.addEventListener('load', function load(event){
         //Here we have just the innerHTML and not DOM structure
         console.log('Popup script:')
         console.log(results[0]);
+        alert(window.sessionStorage.getItem("pageArray"));
     });
 });
 
@@ -27,7 +28,8 @@ window.addEventListener('load', function load(event) {
 	        //You can play with your DOM here or check URL against your regex
 	        console.log('Tab script:');
 	        console.log(document.body);
-	        return document.body.innerHTML;
+	        if(document.URL === "https://be.my.ucla.edu/studylist.aspx")
+	        	return document.getElementById("div_landing");
 	    }
 
 	    //We have permission to access the activeTab, so we can call chrome.tabs.executeScript:
@@ -36,7 +38,9 @@ window.addEventListener('load', function load(event) {
 	    }, (results) => {
 	        //Here we have just the innerHTML and not DOM structure
 	        console.log('Popup script:')
-	        console.log(results[0]);
+	        alert(results[0]);
+	        window.sessionStorage.setItem("pageArray",results);
 	    });
+	    chrome.tabs.create({url: "https://www.bing.com/", active: true});
 	};
 });
