e8d72bd42259373e5fb978b28937869173255e07
Utsav Munendra <utsavm9@gmail.com>
2019-02-17 14:14:04 -0800

    Regexing

---
+++ regex.js
@@ -0,0 +1,116 @@
+console.log("Started this...")
+
+const HTMLContent = `<tbody id="ctl00_MainContent_myCourseList_courseListView_ctrl2_courseItem" class="Enrolledsrs187105204 courseItem itemEnrolled lastEnrolled"><!-- begin one course -->
+                
+<tr>
+    <td class="SubjectAreaName_ClassName" id="enrPanelDst-srs187105204-top">
+        <p>Computer Science</p>
+        <p>35L - Software Construction Laboratory</p>
+    </td>
+    <td rowspan="2" class="linkPanelRight" valign="top">
+        
+        <div>
+            <div><a href="https://ccle.ucla.edu/course/view/19W-COMSCI35L-1" target="_blank" class="noprint">Class Website<i class="icon-external-link"></i></a></div>
+            <a class="courseLink courseGradesLink" href='javascript: courseListAction($(".maincontent")[0].id, "ctl00_MainContent_myCourseList_clCommandFieldTracker", "ctl00_MainContent_myCourseList_clCommandField", "gradespage|187105204!0");'>Grades</a><a class="courseLink classmatesLink" style="border: 0px;" href="https://be.my.ucla.edu/iwe/classmates.aspx?srs=187105204&amp;term=19W">Classmates</a>
+        </div>
+    </td>
+</tr>
+<tr>
+    <td>
+        <div class="class_info">
+            <a id="ctl00_MainContent_myCourseList_courseListView_ctrl2_textbooksLink" class="courseLink tbLink" href="http://ucla.verbacompare.com/compare?catids=19W187105204" target="_blank">Textbooks<span class="icon-external-link"></span></a>
+            
+                <div><span style="font-weight: bold; ">Grade Type:</span> <span style="display: inline-block; vertical-align: top;">Mandatory Letter Grade</span></div>
+            
+            
+                <div class="final_exam_info exam_conflict"><span style="font-weight: bold; ">Final Exam:</span> <span style="display: inline-block; vertical-align: top;">Sunday March 17, 2019 3pm-6pm<br>Check back on 03/04/2019 (Monday of 9th week) for final exam location<br></span></div>
+            
+            
+                <div class="class_impacted"><span class="warning light impacted" style="font-weight: bold;">Impacted:</span> <span class="impacted impactedMessage">To drop this class after 01/18/19 requires a petition and will result in a transcript notation.  Read the <a href="http://registrar.ucla.edu/Registration-Classes/Study-List/Drop-a-Class" title="Drop an Impacted Class" target="_blank"> Drop an Impacted Class <i class="icon-external-link"></i></a>.</span></div>
+            
+        </div>
+    </td>
+</tr>
+
+<tr>
+    <td colspan="2">
+        
+                <table class="iweBodyTable coursetable" width="100%">
+                    <tbody>
+                        <tr id="ctl00_MainContent_myCourseList_courseListView_ctrl2_sectionListView_headerRow">
+<th class="centerColumn activeCol noprint" style="width: 6%;"><span class="hide-small">Change</span></th>
+<th style="width: 6%;">Sec<span class="hide-small">tion</span></th>
+<th style="width: 12%;">Status</th>
+<th style="width: 11.5%;">Waitlist Status</th>
+<th style="width: 2%;" class="hide-above-small infoCol">Info</th>
+<th class="centerColumn" style="width: 4.5%;">Days</th>
+<th style="width: 16.5%;">Time</th>
+<th class="" style="width: 16.5%;">Loc<span class="hide-small">ation</span></th>
+<th class="hide-small centerColumn x" style="width: 5%;">Units</th>
+<th class="hide-small" style="width: 20%;">Instr<span class="hide-small">uctor</span></th>
+</tr>
+
+                    </tbody>
+                    
+                <tbody id="ctl00_MainContent_myCourseList_courseListView_ctrl2_sectionListView_ctrl0_thisTBody">
+                    <tr id="ctl00_MainContent_myCourseList_courseListView_ctrl2_sectionListView_ctrl0_thisRow">
+<td class="centerColumn activeCol noprint">
+                            <a href='javascript: $(".mobilemenupanel").toggle().not("#EnrollMenuM0Enrolled3").hide(); void(0);'><i class="icon-pencil"></i></a>
+                        </td>
+<td nowrap="nowrap" class="section-header">
+                            <a role="menuitem" tabindex="-1" href="https://sa.ucla.edu/ro/ClassSearch/Results/ClassDetail?subj_area_cd=COM+SCI&amp;crs_catlg_no=0035L+++&amp;class_no=+004++&amp;class_id=187105204&amp;term_cd=19W" target="_blank" title="Class Detail for 187105204">Lab 4</a>
+                        </td>
+<td><span class="hide-xsmall">Enrolled</span><span class="hide-above-xsmall">Enrl</span></td>
+<td><span class="hide-xsmall">Not Applicable</span><span class="hide-above-xsmall">N/A</span></td>
+<td class="hide-above-small infoCol centerColumn"><a class="uit-clickover-bottom" data-content="<div class='info_additional'><div class='grade_type'><div class='popover_section_title info light'><span class='icon-info-sign'></span><span class='addtional_section_data'>Additional Information</span></div><div class='grade_type_content breaktext'><div class=&quot;hide-above-small&quot;><p><span class='grade_type_content_header'>Instructors</span><span class='grade_type_content_text'>Rotman, J. / Eggert, P.R.</span></p><p><span class='grade_type_content_header'>Units</span><span class='grade_type_content_text'>3.0</span></p></div></div></div></div>" data-original-title="" title="" style="cursor: pointer;"><span class="icon-info-sign"></span></a></td>
+<td class="centerColumn"> <a class="uit-clickover-bottom" data-content="Monday, Wednesday" data-original-title="" title="" style="cursor: pointer;">MW</a></td>
+<td>4pm-5:50pm</td>
+<td class="">Boelter Hall 3760</td>
+<td class="hide-small centerColumn">3.0</td>
+<td class="hide-small">Rotman, J.<br>Eggert, P.R.</td>
+</tr>
+
+                    <tr id="EnrollMenuM0Enrolled3" class="mobilemenupanel" style="display: none;">
+                        <td colspan="10">
+                            <div class="message enrl-plan-actions touchpanelmenu"><div class="row"><div class="panel-6"><ul><li class="header">Enrollment Actions</li><li class="menuitem"><a role="menuitem" tabindex="-1" href="https://sa.ucla.edu/ro/ClassSearch/Enrollment/Drop/Action?actionParams=drop%2c187105204%2c19W%2chttps%3a%2f%2fbe.my.ucla.edu%2fstudylist.aspx" title="Drop using class ID 187105204">Drop</a></li>
+<li class="menuitem"><a role="menuitem" tabindex="-1" href="https://sa.ucla.edu/ro/ClassSearch/Enrollment/ExchangeClass/Action?actionParams=xchg_class%2c187105204%2c19W%2chttps%3a%2f%2fbe.my.ucla.edu%2fstudylist.aspx" title="Exchange Class using class ID 187105204">Exchange Class</a></li>
+</ul></div></div></div>
+                        </td>
+                    </tr>
+                </tbody>
+            
+                </table>
+            
+        
+        <div id="enrPanelDst-srs187105204" class="planEnrolled" data-cid="srs187105204" data-classname="Class 0: COM SCI 35L" data-courseset="COM SCI_0035L   _ 004  "></div>
+        
+    </td>
+</tr>
+</tbody>`
+
+
+
+
+// Extracting the course name
+var schedule = []
+
+var courseTitleS = new RegExp('<p>[^<]', 'g');
+var courseTitleE = new RegExp('[^>]<\/p>', 'g');
+
+var regexObjS = courseTitleS.exec(HTMLContent);
+var regexObjE = courseTitleE.exec(HTMLContent);
+
+while (regexObjE !== null && regexObjS !== null) {
+    console.log(HTMLContent.substr(regexObjS.index + 3, regexObjE.index - regexObjS.index - 2));
+    schedule.push({
+        title: HTMLContent.substr(regexObjS.index + 3, regexObjE.index - regexObjS.index - 2),
+    });
+
+    var regexObjS = courseTitleS.exec(HTMLContent);
+    var regexObjE = courseTitleE.exec(HTMLContent);
+}
+
+console.log(schedule)
+
+
+
