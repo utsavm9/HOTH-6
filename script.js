af444d11fd16c658cf7ee168564dda3ed5c9c8b0
alexzma <alexanderzma@hotmail.com>
2019-02-17 12:43:33 -0800

    sdf

--- script.js
+++ script.js
@@ -1,5 +1,28 @@
+chrome.tabs.query({active: true, lastFocusedWindow: true}, function(arrayOfTabs) {
+		var tab = arrayOfTabs[0];
+		var url = tab.url;
+		console.log("URL = " + url);
+		chrome.storage.sync.set({"currentTabURL": url}, function(){
+			console.log("currentTabURL: " + url);
+		});
+});
+
+function accessCurrentTabURL(){
+	chrome.storage.sync.get(["currentTabURL"], function(result){
+		console.log("URL of current Tab: " + result.currentTabURL);
+	});
+};
+
+accessCurrentTabURL();
+
 window.addEventListener('load', function load(event) {
     document.getElementById('scanbutton').onclick = function () {
-        alert("This worls? Please say yes");
+        alert(func());
     };
 });
+
+function func(){
+chrome.tabs.getCurrent(function(tab) {
+    alert(tab.title);
+});
+};
