f411bf2ce0ffbc32e7db2ecf9ad6719d7ac3dcb7
alexzma <alexanderzma@hotmail.com>
2019-02-17 13:03:58 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,28 +1,19 @@
-chrome.tabs.query({active: true, lastFocusedWindow: true}, function(arrayOfTabs) {
+function func(){
+	chrome.tabs.query({active: true, lastFocusedWindow: true}, function(arrayOfTabs) {
 		var tab = arrayOfTabs[0];
 		var url = tab.url;
 		console.log("URL = " + url);
 		chrome.storage.sync.set({"currentTabURL": url}, function(){
 			console.log("currentTabURL: " + url);
 		});
-});
-
-function accessCurrentTabURL(){
-	chrome.storage.sync.get(["currentTabURL"], function(result){
-		console.log("URL of current Tab: " + result.currentTabURL);
 	});
 };
 
-accessCurrentTabURL();
-
 window.addEventListener('load', function load(event) {
     document.getElementById('scanbutton').onclick = function () {
-        alert(func());
+        func();
+        chrome.storage.sync.get(["currentTabURL"], function(result){
+		console.log("URL of current Tab: " + result.currentTabURL);
+	});
     };
 });
-
-function func(){
-chrome.tabs.getCurrent(function(tab) {
-    alert(tab.title);
-});
-};
