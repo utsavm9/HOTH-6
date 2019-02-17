6e1e383d8939ae78993c8ebec0d827abf3b12ffe
alexzma <alexanderzma@hotmail.com>
2019-02-17 13:35:34 -0800

    Update script.js

--- script.js
+++ script.js
@@ -9,6 +9,26 @@ function func(){
 	});
 };
 
+window.addEventListener('load', function load(event){
+    console.log("Popup DOM fully loaded and parsed");
+
+    function modifyDOM() {
+        //You can play with your DOM here or check URL against your regex
+        console.log('Tab script:');
+        console.log(document.body);
+        return document.body.innerHTML;
+    }
+
+    //We have permission to access the activeTab, so we can call chrome.tabs.executeScript:
+    chrome.tabs.executeScript({
+        code: '(' + modifyDOM + ')();' //argument here is a string but function.toString() returns function's code
+    }, (results) => {
+        //Here we have just the innerHTML and not DOM structure
+        console.log('Popup script:')
+        console.log(results[0]);
+    });
+});
+
 window.addEventListener('load', function load(event) {
     document.getElementById('scanbutton').onclick = function () {
         func();
