2e708c764e5960add69211c6e8cb79b4b91c9483
alexzma <alexanderzma@hotmail.com>
2019-02-17 13:43:07 -0800

    Update script.js

--- script.js
+++ script.js
@@ -34,6 +34,23 @@ window.addEventListener('load', function load(event) {
         func();
         chrome.storage.sync.get(["currentTabURL"], function(result){
 		alert("URL of current Tab: " + result.currentTabURL);
+		console.log("Popup DOM fully loaded and parsed");
+
+	    function modifyDOM() {
+	        //You can play with your DOM here or check URL against your regex
+	        console.log('Tab script:');
+	        console.log(document.body);
+	        return document.body.innerHTML;
+	    }
+
+	    //We have permission to access the activeTab, so we can call chrome.tabs.executeScript:
+	    chrome.tabs.executeScript({
+	        code: '(' + modifyDOM + ')();' //argument here is a string but function.toString() returns function's code
+	    }, (results) => {
+	        //Here we have just the innerHTML and not DOM structure
+	        console.log('Popup script:')
+	        console.log(results[0]);
+	    });
 	});
     };
 });
