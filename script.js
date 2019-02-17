2c17925d1328659ff0dd30b1ec80eabadd1e16e4
Utsav Munendra <utsavm9@gmail.com>
2019-02-17 12:02:36 -0800

    .

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
 function scan() {
     console.log("This works!!!");
 }
+
+chrome.runtime.onInstalled.addListener(function () {
+    chrome.storage.sync.set({ color: '#3aa757' }, function () {
+        console.log("The color is green.");
+    });
+});
+
+chrome.runtime.onInstalled.addListener(function () {
+    chrome.contextMenus.create({
+        "id": "sampleContextMenu",
+        "title": "Sample Context Menu",
+        "contexts": ["selection"]
+    });
+});
